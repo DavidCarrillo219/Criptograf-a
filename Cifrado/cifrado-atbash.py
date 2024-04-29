757591bbb5ba235da4eeaa4516076025e53f80a8
David Carrillo <david219@ciencias.unam.mx>
2024-04-29 10:10:57 -0600

    Creacion de Carpeta cifrado y del cifrado ATBASH

---
+++ Cifrado/cifrado-atbash.py
@@ -0,0 +1,29 @@
+# cifrado atbash
+import pyperclip
+
+# Alfabetos empleados
+
+claro = "abcdefghijklmnñopqrstuvwxyz"
+cifrado = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+
+# Almacena la forma cifrado/descifrado del texto
+salida = ''
+
+# Guarda el texto introducido 
+
+texto = input("Introduce un texto: ")
+
+# Ejecuta el cifrado/ descifrado letra a letra
+for simbolo in texto.lower():
+    if simbolo in claro:
+        # Identifica la posicion de cada simbolo
+        indice = claro.index(simbolo)
+        # Añade un nuevo simbolo al texto cifrado/descifrado
+        salida += cifrado[indice]
+
+
+# Imprime en pantalla el resultado
+print(salida)
+
+# Copia el mensaje al portapapeles
+pyperclip.copy(salida)
